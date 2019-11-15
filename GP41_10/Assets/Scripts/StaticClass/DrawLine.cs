67927cc328f77a81bccafb7af95ed4b8a2bcb185
amano_satoshi <satoshi.hal.2019@gmail.com>
2019-11-15 14:51:16 +0900

    プロトタイプ作成

---
+++ GP41_10/Assets/Scripts/StaticClass/DrawLine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DrawLine
+{
+    public static void Draw(Vector3 start, Vector3 end, float size, Color color)
+    {
+        GameObject newLine = new GameObject("Line");
+        LineRenderer lRend = newLine.AddComponent<LineRenderer>();
+        lRend.startWidth = size;
+        lRend.endWidth = size;
+        lRend.material.color = color;
+        lRend.SetPosition(0, start);
+        lRend.SetPosition(1, end);
+    }
+}
