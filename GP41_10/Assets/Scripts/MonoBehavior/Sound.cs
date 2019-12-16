81c64ab623ccf74dd83707a5a5eae12301280e4b
Darakesan <oosumigod@gmail.com>
2019-12-16 11:50:14 +0900

    PlayerSelectシーンのcanvas修正。キーパッドでのSE追加

--- GP41_10/Assets/Scripts/MonoBehavior/Sound.cs
+++ GP41_10/Assets/Scripts/MonoBehavior/Sound.cs
@@ -7,18 +7,26 @@ public class Sound : MonoBehaviour
     public int nNum;        // 0:タイトル       1:人数選択
                             // 2:メイン         3:リザルト
 
+    private bool bKey;
+
     private AudioSource[] sources;
     void Start()
     {
         sources = gameObject.GetComponents<AudioSource>();
         sources[0].Play();      // BGM
         Debug.Log(sources[0].name + "再生");
+
+        bKey = false;
     }
 
 
     void Update()
     {
-        switch(nNum)
+        float pd1h = Input.GetAxis("Pad1_H");
+         //L Stick
+        float pd1lsh = Input.GetAxis("Pad1_LStick_H");
+
+        switch (nNum)
         {
             case 0:
                 if (Input.GetKeyDown(KeyCode.Return))
@@ -28,14 +36,29 @@ public class Sound : MonoBehaviour
                 break;
 
             case 1:
-                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+                if ((pd1lsh < -0.5f ||
+                    pd1h > 0.3f ||
+                    Input.GetKeyDown(KeyCode.W))
+                    &&
+                    bKey)
                 {
                     sources[1].Play();
+                    Debug.Log("CURSOR選択音");
+                    bKey = false;
+                }
+                else if ((pd1lsh > 0.5f ||
+                        pd1h < -0.3f
+                           || Input.GetKeyDown(KeyCode.S)) &&
+                    !bKey)
+                {
+                    sources[2].Play();
+                    Debug.Log("CURSOR選択音");
+                    bKey = true;
                 }
 
                 if (Input.GetKeyDown(KeyCode.Return))
                 {
-                    sources[2].Play();
+                    sources[3].Play();
                 }
                 break;
 
