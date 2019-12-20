58be9d1150a946bfa220c262305823b95ce927a4
amano_satoshi <satoshi.hal.2019@gmail.com>
2019-12-20 20:38:27 +0900

    おぼれた人の挙動を違和感ないように変更

--- GP41_10/Assets/Scripts/MonoBehavior/TorpedoBubble.cs
+++ GP41_10/Assets/Scripts/MonoBehavior/TorpedoBubble.cs
@@ -6,6 +6,9 @@ public class TorpedoBubble : MonoBehaviour
 {
     public GameObject TorpedoBubbleObj;
     private GameObject torpedoBubble;
+    [SerializeField, Header("泡発射間隔")]
+    private float bubbleTime = 0f;
+    private float CurrentTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,12 @@ public class TorpedoBubble : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        torpedoBubble = Instantiate(TorpedoBubbleObj, transform.position, transform.rotation);
-        torpedoBubble.transform.localScale *= -1;
+        CurrentTime += Time.deltaTime;
+        if(CurrentTime >= bubbleTime)
+        {
+            torpedoBubble = Instantiate(TorpedoBubbleObj, transform.position, transform.rotation);
+            torpedoBubble.transform.localScale *= -1;
+            CurrentTime = 0f;
+        }
     }
 }
